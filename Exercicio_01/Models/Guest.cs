1ba76b247c3c7641dfdf332afd7c792e90a9ed38
rodrigoreisdev <67196386+rodrigoreisdev@users.noreply.github.com>
2021-02-18 20:11:58 -0300

    Classe Guest

---
+++ Exercicio_01/Models/Guest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Exercicio_01.Models
+{
+    public class Guest
+    {
+        public string Nome { get; set; }
+        public string EMail { get; set; }
+        public string Telefone { get; set; }
+        public bool? comparecimento { get; set; }
+    }
+}
