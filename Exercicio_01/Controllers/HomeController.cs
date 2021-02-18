223b9388f24e7cd7af0c21d6f0d1e4fdd72fca42
Antonio <antonioalves055@hotmail.com>
2021-02-18 19:45:37 -0300

    First commit
    First commit

---
+++ Exercicio_01/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Exercicio_01.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Exercicio_01.Controllers {
+    public class HomeController : Controller {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger) {
+            _logger = logger;
+        }
+
+        public IActionResult Index() {
+            return View();
+        }
+
+        public IActionResult Privacy() {
+            return View();
+        }
+    }
+}
