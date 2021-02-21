5ac5d65cfa342aed971fb52db045aa9c30077bf3
Antonio <antonioalves055@hotmail.com>
2021-02-21 17:37:08 -0300

    Adding views
    Adding views of all methods

--- Exercicio_01/Controllers/GuestsController.cs
+++ Exercicio_01/Controllers/GuestsController.cs
@@ -7,38 +7,80 @@ using System.Threading.Tasks;
 using Exercicio_01.Models;
 namespace Exercicio_01.Controllers {
     public class GuestsController : Controller {
-        private List<Guest> guests = new List<Guest>();
+        private static IList<Guest> ListGuests = new List<Guest>
+            {
+                new Guest()
+                {
+                    Id = 1,
+                    Nome = "Notebooks",
+                    Email = "Teste",
+                    Telefone = "teste"
+                },
+
+                new Guest()
+                {
+                    Id = 2,
+                    Nome = "Monitores",
+                    Email = "Teste",
+                    Telefone = "teste"
+                },
+
+                new Guest()
+                {
+                    Id = 3,
+                    Nome = "Impressoras",
+                    Email = "Teste",
+                    Telefone = "teste"
+                },
+
+                new Guest()
+                {
+                    Id = 4,
+                    Nome = "Mouses",
+                    Email = "Teste",
+                    Telefone = "teste"
+                },
+
+                new Guest()
+                {
+                    Id = 5,
+                    Nome = "Desktops",
+                    Email = "Teste",
+                    Telefone = "teste"
+
+                }
+        };
         public IActionResult Index() {
-            return View();
+            return View(ListGuests);
         }
         public IActionResult Create() {
             return View();
         }
-        public IActionResult Details() {
-            return View();
+        public IActionResult Details(long id) {
+            return View(ListGuests.Where(m => m.Id == id).First());
         }
-        public IActionResult Edit() {
-            return View();
+        public IActionResult Edit(long id) {
+            return View(ListGuests.Where(m => m.Id == id).First()); ;
         }
-        public IActionResult Delete() {
-            return View();
+        public IActionResult Delete(long id) {
+            return View(ListGuests.Where(m => m.Id == id).First());
         }
 
-        //[HttpPost]
-        //public ActionResult Details(Guest guest) {
-
-        //}
-        //[HttpPost]
-        //public ActionResult Edit(Guest guest) {
-
-        //}
-        //[HttpPost]
-        //public ActionResult Delete(Guest guest) {
-
-        //}
+        [HttpPost]
+        public ActionResult Edit(Guest guest) {
+            ListGuests.Remove(ListGuests.Where(c => c.Id == guest.Id).First());
+            ListGuests.Add(guest);
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public ActionResult Delete(Guest guest) {
+            ListGuests.Remove(ListGuests.Where(c => c.Id == guest.Id).First());
+            return RedirectToAction("Index");
+        }
         [HttpPost]
         public IActionResult Create(Guest guest) {
-            guests.Add(guest);
+            ListGuests.Add(guest);
+            guest.Id = ListGuests.Select(m => m.Id).Max() + 1;
             return RedirectToAction("Index");
         }
     }
--- Exercicio_01/Models/Guest.cs
+++ Exercicio_01/Models/Guest.cs
@@ -7,6 +7,7 @@ namespace Exercicio_01.Models
 {
     public class Guest
     {
+        public int Id { get; set; }
         public string Nome { get; set; }
         public string Email { get; set; }
         public string Telefone { get; set; }
